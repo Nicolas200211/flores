71544c861bd753e65086b7eee7d8d9309918e7d6
Nicolas200211 <nico_e123@outlook.com>
2025-07-31 21:54:42 -0500

    Configuración para Render

---
+++ app.py
@@ -0,0 +1,9 @@
+from tulipanes import *
+import os
+
+# Iniciar la aplicación
+try:
+    tu.mainloop()
+except Exception as e:
+    print(f"Error: {str(e)}")
+    input("Presiona Enter para salir...")
